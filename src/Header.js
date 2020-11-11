2874cb7e44c4c72809d4d6fc873dd5949810fd5c
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2020-11-10 22:15:25 -0700

    add styling

--- src/Header.js
+++ src/Header.js
@@ -5,19 +5,22 @@ class Header extends Component {
     render() {
         return (
         
-                <header className="header-container">
-                    <Link to='/'>
-                        <h1 className="nav-item">Home</h1>
-                    </Link>
-                    <Link to='/about'>
-                        <h1 className="nav-item">About Me</h1>
-                    </Link>
-                    <Link to='/projects'>
-                        <h1 className="nav-item">Projects</h1>
-                    </Link>
-                    <Link to='/skills'>
-                        <h1 className="nav-item">Skills</h1>
-                    </Link>
+                <header>
+                    <nav className="header-container">
+                        <Link to='/'>
+                            <h1 className="nav-item">Home</h1>
+                        </Link>
+                        <Link to='/about'>
+                            <h1 className="nav-item">About Me</h1>
+                        </Link>
+                        <Link to='/projects'>
+                            <h1 className="nav-item">Projects</h1>
+                        </Link>
+                        <Link to='/skills'>
+                            <h1 className="nav-item">Skills</h1>
+                        </Link>
+                    </nav>
+                    
                 </header>
             
         );
